841c7b4828e2f1d1d105a0d0d2de9960b8dd0458
Elene Sturua <sturuael@grinnell.edu>
2025-08-01 23:20:07 -0500

    - getters and setters added

--- src/Task.java
+++ src/Task.java
@@ -1,8 +1,73 @@
 public class Task {
-    private String taskname;
-    private int taskid;
+    private String taskName;
+    private int taskId;
     private int taskPriority;
     private String subject;
     private LocalDate dueDate;
-    private boolean completed
+    private boolean completed;
 }
+
+public Task(String taskName, int taskId, int taskPriority, String subject, LocalDate dueDate){
+    this.taskName=taskName;
+    this.taskId=taskId;
+    this.taskPriority=taskPriority;
+    this.subject=subject;
+    this.dueDate=dueDate;
+    this.completed=false;
+}
+
+
+
+public String getGetTaskName() {
+    return taskName;
+}
+public int getTaskId(){
+    return taskId;
+}
+
+public int getTaskPriority(){
+    return taskPriority;
+}
+public String subject(){
+    return subject;
+}
+
+public getDueDate() {
+    return dueDate;
+}
+
+public boolean isCompleted (){
+    return isCompleted;
+}
+
+public void setTaskId(int taskId) {
+    this.taskId = taskId;
+}
+
+public void setTaskPriority(int taskPriority) {
+    this.taskPriority = taskPriority;
+}
+
+public void setSubject(String subject) {
+    this.subject = subject;
+}
+
+public void setDueDate(LocalDate dueDate) {
+    this.dueDate = dueDate;
+}
+
+public void setCompleted(boolean completed) {
+    this.completed = completed;
+}
+
+public void markComplete() {
+    this.completed = true;
+}
+
+@Override
+public String toString(){
+    return String.format(" completed ");
+}
+
+
+
