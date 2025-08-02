a25b6a9dfc3b38372e22075dbf6cfe8be24f48b6
Elene Sturua <sturuael@grinnell.edu>
2025-08-01 23:01:40 -0500

    - defined the task object class including all the inputs to be prompted from the user for planning

---
+++ src/Task.java
@@ -0,0 +1,8 @@
+public class Task {
+    private String taskname;
+    private int taskid;
+    private int taskPriority;
+    private String subject;
+    private LocalDate dueDate;
+    private boolean completed
+}
